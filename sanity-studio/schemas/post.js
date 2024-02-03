e9d43e4b7014a2e39538635edc98ee41558fc820
Jaewoong <dixk3458@naver.com>
2024-02-03 21:14:06 +0900

    Sanity Studio-preview list만들기

--- sanity-studio/schemas/post.js
+++ sanity-studio/schemas/post.js
@@ -52,4 +52,21 @@ export default {
       ],
     },
   ],
+  preview: {
+    select: {
+      title: 'comments.0.comment',
+      authorName: 'author.name',
+      authorUserName: 'author.username',
+      media: 'photo',
+    },
+    prepare(selection) {
+      const {title, authorName, authorUserName, media} = selection
+
+      return {
+        title: title,
+        media: media,
+        subtitle: `by ${authorName} (${authorUserName})`,
+      }
+    },
+  },
 }
