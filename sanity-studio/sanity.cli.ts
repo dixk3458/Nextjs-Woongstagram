8af9e61227bb07cf17df10c23c4dd0f323a934d5
Jaewoong <dixk3458@naver.com>
2024-02-28 15:25:17 +0900

    Config 수정

--- sanity-studio/sanity.cli.ts
+++ sanity-studio/sanity.cli.ts
@@ -3,6 +3,6 @@ import {defineCliConfig} from 'sanity/cli'
 export default defineCliConfig({
   api: {
     projectId: 'du3wfl58',
-    dataset: 'production'
-  }
+    dataset: 'production',
+  },
 })
