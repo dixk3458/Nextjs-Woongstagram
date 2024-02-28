fed9e40eb3ff7e36f6089e675c68031b51cae9ab
Jaewoong <dixk3458@naver.com>
2024-02-28 15:49:28 +0900

    Config 수정

--- sanity-studio/sanity.config.ts
+++ sanity-studio/sanity.config.ts
@@ -1,4 +1,4 @@
-import {defineConfig} from 'sanity'
+import {defineConfig} from 'sanity/lib/exports'
 import {structureTool} from 'sanity/structure'
 import {visionTool} from '@sanity/vision'
 import {schemaTypes} from './schemas'
